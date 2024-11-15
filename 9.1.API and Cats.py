144c15fe83251e43c99517ae14cb41486fefa2a6
ivanranet <ivanranet@yandex.ru>
2024-11-15 14:35:33 +0500

    добавил загрузку каждой картинки в новом окне

--- 9.1.API and Cats.py
+++ 9.1.API and Cats.py
@@ -9,16 +9,20 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img_local = Image.open(image_data)
-        img_local.thumbnail((600, 480), Image.Resampling.LANCZOS)
+        img_local.thumbnail((600, 500), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img_local)
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return  None
 
-def set_image():
+def open_new_window():
     img = load_image(url)
     if img:
-        label.config(image = img)
+        new_window = Toplevel()
+        new_window.title('Картинка котика')
+        new_window.geometry('600x500')
+        label = Label(new_window, image = img)
+        label.pack()
         label.image = img
 
 def exit():
@@ -28,23 +32,15 @@ window = Tk()
 window.title('Cats!')
 window.geometry('600x520')
 
-label = Label()
-label.pack()
-
-# update_button = Button(text = 'Обновить', command = set_image)
-# update_button.pack()
-
 menu_bar = Menu(window)
 window.config(menu = menu_bar)
 
 file_menu = Menu(menu_bar, tearoff = 0)
 menu_bar.add_cascade(label = 'Файл', menu = file_menu)
-file_menu.add_command(label = 'Загрузить фото', command = set_image)
+file_menu.add_command(label = 'Загрузить фото', command = open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label = 'Выход', command = exit)
 
 url = 'https://cataas.com/cat'
-
-set_image()
 
 window.mainloop()
