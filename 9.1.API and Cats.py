c2649c3e55940f82c50decaccebedfee81840be4
ivanranet <ivanranet@yandex.ru>
2024-11-15 11:09:10 +0500

    добавил кнопку загрузки нового изображения

--- 9.1.API and Cats.py
+++ 9.1.API and Cats.py
@@ -3,20 +3,22 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from bottle import response
-from pygame.examples.cursors import image
-
-
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img_local = Image.open(image_data)
-        return ImageTk.PhotoImage(img)
+        return ImageTk.PhotoImage(img_local)
     except Exception as e:
         print(f'Произошла ошибка: {e}')
         return  None
+
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image = img)
+        label.image = img
 
 window = Tk()
 window.title('Cats!')
@@ -25,11 +27,11 @@ window.geometry('600x480')
 label = Label()
 label.pack()
 
-url = 'https://cataas.com/cat'
-img = load_image(url)
+update_button = Button(text = 'Обновить', command = set_image)
+update_button.pack()
 
-if img:
-    label.config(image = img)
-    label.image = img
+url = 'https://cataas.com/cat'
+
+set_image()
 
 window.mainloop()
